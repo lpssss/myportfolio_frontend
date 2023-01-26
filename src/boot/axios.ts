82de4a6658ab3734e0f57c26b2fe27db2973c828
lpssss <lim55570@gmail.com>
2023-01-26 11:44:32 +0800

    Add port to URL

--- src/boot/axios.ts
+++ src/boot/axios.ts
@@ -13,7 +13,7 @@ declare module '@vue/runtime-core' {
 // good idea to move this instance creation inside of the
 // "export default () => {}" function below (which runs individually
 // for each client)
-const api = axios.create({ baseURL: 'https://portfolio.lpssss.online/api' });
+const api = axios.create({ baseURL: 'https://portfolio.lpssss.online:8443/api' });
 
 export default boot(({ app }) => {
   // for use inside Vue files (Options API) through this.$axios and this.$api
